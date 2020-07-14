07554a01ce4ae3810ce28f2a164cbcf91ae483f9
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-07-14 18:37:39 -0300

    Add Edge class

--- Edge.h
+++ Edge.h
@@ -1,14 +1,36 @@
-//
-// Created by valentina on 18/6/20.
-//
-
 #ifndef GRAPHTEMPLATES_EDGE_H
 #define GRAPHTEMPLATES_EDGE_H
 
+#include "Vertex.h"
 
+template <class Type> class Vertex;
+
+template <typename Type>
 class Edge {
 
+    private:
+        Vertex<Type>* end;
+
+    public:
+        Edge(Vertex<Type>* end);
+        Type getEndVertexData();
+        Vertex<Type>* getEndVertex();
 };
 
+///////////////////////////////////////////// IMPLEMENTATION /////////////////////////////////////////////
+template <typename Type>
+Edge<Type>:: Edge(Vertex<Type> *end) {
+    this->end = end;
+}
+
+template <typename Type>
+Type Edge<Type>:: getEndVertexData() {
+    return end->getKey();
+}
+
+template <typename Type>
+Vertex<Type>* Edge<Type>:: getEndVertex() {
+    return end;
+}
 
 #endif //GRAPHTEMPLATES_EDGE_H
