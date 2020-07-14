628c2840ff40a9f7327faab2e43ef55d128b937a
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-07-14 18:37:47 -0300

    Add Vertex class

--- Vertex.h
+++ Vertex.h
@@ -1,14 +1,70 @@
-//
-// Created by valentina on 18/6/20.
-//
-
 #ifndef GRAPHTEMPLATES_VERTEX_H
 #define GRAPHTEMPLATES_VERTEX_H
 
+#include "Edge.h"
+#include "List.h"
 
+template <typename Type>
 class Vertex {
 
+    private:
+        List<Edge<Type>*> edges;
+        Type key;
+        bool visited;
+
+    public:
+        Vertex();
+        Vertex(Type key);
+        Type getKey();
+        bool getVisited();
+        List<Vertex<Type>*> getEdges();
+        void setKey(Type key);
+        void setVisited(bool visited);
+        void addAdjEdge(Edge<Type>* edge);
 };
+
+///////////////////////////////////////////// IMPLEMENTATION /////////////////////////////////////////////
+template <typename Type>
+Vertex<Type>:: Vertex(Type key) {
+    this->key = key;
+}
+
+/////////////////////////////////////////////
+template <typename Type>
+Type Vertex<Type>:: getKey() {
+    return key;
+}
+
+/////////////////////////////////////////////
+template <typename Type>
+bool Vertex<Type>:: getVisited() {
+    return visited;
+}
+
+/////////////////////////////////////////////
+template <typename Type>
+List<Vertex<Type>*> Vertex<Type>:: getEdges() {
+    return edges;
+}
+
+/////////////////////////////////////////////
+template <typename Type>
+void Vertex<Type>:: setKey(Type key) {
+    this->key = key;
+}
+
+/////////////////////////////////////////////
+template <typename Type>
+void Vertex<Type>:: setVisited(bool visited) {
+    this->visited = visited;
+}
+
+/////////////////////////////////////////////
+template <typename Type>
+void Vertex<Type>:: addAdjEdge(Edge<Type>* edge) {
+    edges.insertAtEnd(edge);
+}
+
 
 
 #endif //GRAPHTEMPLATES_VERTEX_H
