d5497f13f82c928bf31b8111ae3d8a0e755048ec
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-07-20 16:58:33 -0300

    feature: Simple test

--- main.cpp
+++ main.cpp
@@ -6,34 +6,42 @@ using namespace std;
 typedef int type;
 int main() {
 
-    Graph<type>* graph = new Graph<type>(1,2);
-    graph->addEdge(1,2);
-    graph->addEdge(2,1);
-    graph->addEdge(2,3);
-    graph->addEdge(3,1);
+    cout << "\n\t----------------------------------------------------------------\n";
+
+    Graph<type>* graph = new Graph<type>(1,2,5);
+    graph->addEdge(1,2,4);
+    graph->addEdge(2,1,3);
+    graph->addEdge(2,3,14);
+    graph->addEdge(3,1,7);
+
+    cout << "\t----------------------------------------------------------------\n";
 
     cout << boolalpha;
-    cout << "\n\tV1 to V2: " << graph->existsEdge(1,2) << endl;
-    cout << "\tV2 to V1: " << graph->existsEdge(2,1) << endl;
-    cout << "\tV2 to V3: " << graph->existsEdge(2,3) << endl;
-    cout << "\tV1 to V3: " << graph->existsEdge(1,3) << endl;
-    cout << "\tV3 to V2: " << graph->existsEdge(3,2) << endl;
+    cout << "\t\tV1 to V2: " << graph->existsEdge(1,2) << " - Cost: " << graph->getEdge(1,2)->getCost() << endl;
+    cout << "\t\tV2 to V1: " << graph->existsEdge(2,1) << " - Cost: " << graph->getEdge(2,1)->getCost() << endl;
+    cout << "\t\tV2 to V3: " << graph->existsEdge(2,3) << " - Cost: " << graph->getEdge(2,3)->getCost() << endl;
+    cout << "\t\tV3 to V1: " << graph->existsEdge(3,1) << " - Cost: " << graph->getEdge(3,1)->getCost() <<  endl;
+    cout << "\t\tV1 to V3: " << graph->existsEdge(1,3) << endl;
 
-    cout << "\n\tV1 exists: " << graph->existsVertex(1) << endl;
-    cout << "\tV2 exists: " << graph->existsVertex(2) << endl;
-    cout << "\tV3 exists: " << graph->existsVertex(3) << endl;
-    cout << "\tV4 exists: " << graph->existsVertex(4) << endl;
+    cout << "\t----------------------------------------------------------------\n";
 
-    cout << "\n\tV1 <- E2-E1 -> V2\n"
-                "\t /\\            |\n"
-                "\t  \\            |\n"
-                "\t    \\          |\n"
-                "\t      \\       E3\n"
-                "\t        \\      |\n"
-                "\t          \\    |\n"
-                "\t            \\ \\/\n"
-                "\t              V3\n";
+    cout << "\t\tV1 exists: " << graph->existsVertex(1) << endl;
+    cout << "\t\tV2 exists: " << graph->existsVertex(2) << endl;
+    cout << "\t\tV3 exists: " << graph->existsVertex(3) << endl;
+    cout << "\t\tV4 exists: " << graph->existsVertex(4) << endl;
 
+    cout << "\t----------------------------------------------------------------\n";
+
+    cout << "\t\tV1 <- E2-E1 -> V2\n"
+            "\t\t /\\            |\n"
+            "\t\t  \\            |\n"
+            "\t\t    \\          |\n"
+            "\t\t      \\       E3\n"
+            "\t\t        \\      |\n"
+            "\t\t          \\    |\n"
+            "\t\t            \\ \\/\n"
+            "\t\t              V3\n";
+    cout << "\t----------------------------------------------------------------\n";
     delete graph;
     return 0;
 }
